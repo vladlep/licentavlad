12c8618ec7d4843402d5a5306f960d0dc0f2914f
vlad.lep <vlad.lep@122ac476-ae23-22c3-0b7d-92a7310358e7>
2010-12-27 19:28:47 +0000

    Exemplul java pentru care am generat factbase-ul.

---
+++ java/testproject1/src/A.java
@@ -0,0 +1,25 @@
+public class A{
+	
+	public void methodA1()
+	{
+		
+		if(true)
+		{
+		B b = new B();
+		b.methodB2();
+			
+		}
+		
+	}
+	
+	public void methodA2()
+	{
+		
+	}
+	
+	public void methodA3()
+	{
+		
+	}
+	
+}
