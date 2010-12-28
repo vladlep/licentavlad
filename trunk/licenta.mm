73cbac48a7d1a3f7b2f5d94613b27516cf518f9f
vlad.lep <vlad.lep@122ac476-ae23-22c3-0b7d-92a7310358e7>
2010-12-28 20:56:22 +0000

    the plan

---
+++ trunk/licenta.mm
@@ -0,0 +1,99 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1293481635953" ID="Freemind_Link_270099476" MODIFIED="1293482545828" TEXT="licenta">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<node COLOR="#0033ff" CREATED="1293481963765" ID="Freemind_Link_407792461" MODIFIED="1293482398234" POSITION="right" TEXT="dev steps : ">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1293481663421" ID="_" MODIFIED="1293482539109" TEXT=" creez arbori noi cu metode pornind de la call-uri">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1293482295812" ID="Freemind_Link_1174414198" MODIFIED="1293482398234" TEXT="retin o lista de passed calluri ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1293482323187" ID="Freemind_Link_330959467" MODIFIED="1293482398218" TEXT=" rezolva asta problema recursivitatii?"/>
+</node>
+<node COLOR="#990000" CREATED="1293482303921" ID="Freemind_Link_418164863" MODIFIED="1293482398234" TEXT="un call leaga 2 metode ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1293481799218" ID="Freemind_Link_134096361" MODIFIED="1293482398218" TEXT=" creez abrori pentru 2 factbase-uri">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1293481688953" ID="Freemind_Link_1942693815" MODIFIED="1293482398218" TEXT=" compar arborii si gasesc daca sunt comuni / nu">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1293481706859" ID="Freemind_Link_1621273347" MODIFIED="1293482398218" TEXT="optional adaug metrici in plus pe langa arbosri">
+<font ITALIC="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="help"/>
+<node COLOR="#990000" CREATED="1293481725187" ID="Freemind_Link_839074219" MODIFIED="1293482398218" TEXT="metrici la nivel de clasa ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1293481749437" ID="Freemind_Link_992388798" MODIFIED="1293482398218" TEXT="nr metode"/>
+<node COLOR="#111111" CREATED="1293481754078" ID="Freemind_Link_629929343" MODIFIED="1293482398218" TEXT="nr atribute"/>
+</node>
+<node COLOR="#990000" CREATED="1293481731218" ID="Freemind_Link_861534075" MODIFIED="1293482398218" TEXT="metrici la nivel de metoda ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1293481737000" ID="Freemind_Link_774984234" MODIFIED="1293482398218" TEXT="nr operatori"/>
+<node COLOR="#111111" CREATED="1293481741000" ID="Freemind_Link_467380151" MODIFIED="1293482398218" TEXT="nr if , while, "/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1293481789703" ID="Freemind_Link_510121757" MODIFIED="1293482398218" TEXT="optional  automatizez too-ul. Citesc dintr-un fisier  root_dir si multe factbase-uri, compar 2 cate 2  :  n*(n-1)/2 variante">
+<font ITALIC="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1293481921671" ID="Freemind_Link_276005929" MODIFIED="1293482510328" POSITION="left" TEXT="Questions">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1293482002078" ID="Freemind_Link_601417407" MODIFIED="1293482398218" TEXT="cum deschid mai multe fisiere in memorie">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1293482023406" ID="Freemind_Link_1793782544" MODIFIED="1293482398187" TEXT="bookmarked varaianta.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1293482035109" ID="Freemind_Link_1736593503" MODIFIED="1293482398187" TEXT="deschid in module ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1293482048843" ID="Freemind_Link_1115668120" MODIFIED="1293482398187" TEXT="cum fac asta ? "/>
+<node COLOR="#111111" CREATED="1293482053281" ID="Freemind_Link_1937936634" MODIFIED="1293482398171" TEXT="nu se incarca prea mult memoria ?"/>
+</node>
+<node COLOR="#990000" CREATED="1293482062421" ID="Freemind_Link_892328601" MODIFIED="1293482398187" TEXT="gasesc comanda de un-consult">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1293563864312" ID="Freemind_Link_1252245883" MODIFIED="1293563898890" TEXT="retractall(nod(A1,A2,...A7)). --- pentru fiecare tip ce ma intereseaza">
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1293482072125" ID="Freemind_Link_691215201" MODIFIED="1293482398171" TEXT="citesc fisierul cu read , intr-o lista si o pases ca parametru prin program ?">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1293482125765" ID="Freemind_Link_438930954" MODIFIED="1293482398171" TEXT="trebuie sa sterg lista oricum cu o comanda"/>
+<node COLOR="#111111" CREATED="1293482175468" ID="Freemind_Link_1026536198" MODIFIED="1293482398171" TEXT="abolish ? verific daca merge"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1293482183703" ID="Freemind_Link_1010909261" MODIFIED="1293482398171" TEXT="cum compar arborii ">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1293482204781" ID="Freemind_Link_598880407" MODIFIED="1293482398171" TEXT="cum reprezint arborii ? e mai avantajos sa ii unesc sau nu ?">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1293482218031" ID="Freemind_Link_1986259140" MODIFIED="1293482398171" TEXT="problema recursivitatii ">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1293482231312" ID="Freemind_Link_1836679013" MODIFIED="1293482398171" TEXT="cum o detectez">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1293482245156" ID="Freemind_Link_1910266827" MODIFIED="1293482398156" TEXT="ma opresc cand o detectez din a construi arborele.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1293482190859" ID="Freemind_Link_1790137954" MODIFIED="1293482398156" TEXT="cum afisez rezultatele">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1293482829984" ID="Freemind_Link_1890660152" MODIFIED="1293482840781" POSITION="right" TEXT="not sure if useful yet" VSHIFT="72">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1293482841890" ID="Freemind_Link_247547404" MODIFIED="1293482873421" TEXT="listing command -&gt; write database to file">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1293482875250" ID="Freemind_Link_1228317460" MODIFIED="1293482886062" TEXT="see bookM 2">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
