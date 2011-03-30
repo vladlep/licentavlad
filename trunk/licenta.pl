e6c209d40c644e3f0a39882cc8364b963c658295
vlad.lep <vlad.lep@122ac476-ae23-22c3-0b7d-92a7310358e7>
2011-03-30 18:45:51 +0000

    NO Starting point. Prior implementation will not be used. No algorithm ideea. New factbase

--- trunk/licenta.pl
+++ trunk/licenta.pl
@@ -22,12 +22,57 @@ run(X):-
 	assert(nod(Z,B,[])),
 
 	writef(B).
+%return the number of elements form a list. The result is attached to
+%the variable NrOfElements
+count([],0).
+count([_|Tail],NrOfElements) :- count(Tail,Left), NrOfElements is Left+1.
+
+% calculez care e nr de atribute pentru o clasa dintr-un modul
+calcNrAtrib(Modul,IdClasa,Nr) :-
+	module(Modul),
+	findall(X,fieldT(X,IdClasa,_,_,_),Result),
+	count(Result,Nr).
+
+
+
+% calculez care e nr de metode pentru o clasa dintr-un modul
+
+
+%incarca un proiect a carui nume e trimis ca parametru intr-un modul cu
+%acelasi nume.
+%
+load(Prj):-
+	module(Prj),
+	atom_concat('c:/users/vll/vlad/licentavlad/',Prj,Result),
+	consult(Result).
+
+%	consult('c:/users/vll/vlad/licentavlad/licenta.pl'),
+%	module(prj2),
+%	consult('c:/users/vll/vlad/licentavlad/licenta.pl'),
+%	consult('c:/users/vll/vlad/licentavlad/stockMarket.pl').
+
+
+
+comapareClassLevel(Prj1,ClassID1,Prj2,ClassID2).
+
+%The main function that compares 2 project.
+%Prj1 - in param. Name of first project
+%Prj2 - in param. Name of second project
+
+compare2Prj(Prj1,Prj2) :-
+	load(Prj1),
+	load(Prj2),
+
+	comapareClassLevel(Prj1,ClassID1,Prj2,ClassID2).
+
 
 
 main(Z):- open('I:/vlad/serios/faculta/licenta/licentavlad/test1.pl', read, Z), set_output(Z), listing,
 set_output(screen), close(Z).
 
 loadFile(File):- consult(File).
+
+
 
 clearDatabase:-
 	retractall(classT(_,_,_,_)),
@@ -56,6 +101,22 @@ calcTree(File,Tree):- loadFile(File),
 
 
 testCalc(Tree):- calcTree('C:/Users/vll/vlad/licentavlad/test1.pl',Tree).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
