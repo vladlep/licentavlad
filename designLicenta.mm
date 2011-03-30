21c8ea20c980e4c820c842435ee0e04bc2569cf3
vlad.lep <vlad.lep@122ac476-ae23-22c3-0b7d-92a7310358e7>
2011-03-30 18:46:41 +0000

    new designLicenta.mm - designul pentru primul increment al licentei

---
+++ designLicenta.mm
@@ -0,0 +1,53 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1301509237098" ID="Freemind_Link_218113927" MODIFIED="1301510758714" TEXT="design">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1301509265173" ID="_" MODIFIED="1301510759197" POSITION="right" TEXT="compare2Prj(PrjName1,PrjName2)">
+<font NAME="SansSerif" SIZE="18"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+<node COLOR="#00b439" CREATED="1301509295544" ID="Freemind_Link_413715704" MODIFIED="1301510758714" TEXT="load (PrjName1)">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+<node COLOR="#990000" CREATED="1301509427798" ID="Freemind_Link_1854615329" MODIFIED="1301510759197" TEXT="incarca in modului cu acelasi nume ca si numele proiectului, baza de cunostinte creata pentru proiectul trimis ca parametri">
+<font NAME="SansSerif" SIZE="14"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1301509334177" ID="Freemind_Link_920811095" MODIFIED="1301510758714" TEXT="load (PrjName2)">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+</node>
+<node COLOR="#00b439" CREATED="1301509530080" ID="Freemind_Link_1363009387" MODIFIED="1301510759197" TEXT="compareClassLevel(ProjName1,ClassId1,ProjName2, ClassId2)">
+<font NAME="SansSerif" SIZE="16"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+<node COLOR="#990000" CREATED="1301509355682" ID="Freemind_Link_412991237" MODIFIED="1301510758854" TEXT="myClass(PrjName1, ClassId1)">
+<font NAME="SansSerif" SIZE="14"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+</node>
+<node COLOR="#990000" CREATED="1301509391796" ID="Freemind_Link_914860179" MODIFIED="1301510758823" TEXT="myClass(PrjName1, ClassId2) ">
+<font NAME="SansSerif" SIZE="14"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+</node>
+<node COLOR="#990000" CREATED="1301509609289" ID="Freemind_Link_1100629602" MODIFIED="1301510758854" TEXT="calcNrAtrib (PrjName1, ClassId1,Nr)">
+<font NAME="SansSerif" SIZE="14"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+</node>
+<node COLOR="#990000" CREATED="1301509692936" ID="Freemind_Link_1453185276" MODIFIED="1301510758823" TEXT="calcNrAtrib (PrjName2, ClassId2,Nr) ">
+<font NAME="SansSerif" SIZE="14"/>
+<hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
+</node>
+<node COLOR="#990000" CREATED="1301509767965" ID="Freemind_Link_1020799320" MODIFIED="1301509781523" TEXT="... pentr restul metricilor la nivel de clasa">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1301509785117" ID="Freemind_Link_110552313" MODIFIED="1301509820812" TEXT="compareMethods(ProjName1,ClassId1,ProjName2, ClassId2)">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1301509822222" ID="Freemind_Link_1040224794" MODIFIED="1301509843752" TEXT="compareCallDepend(ProjName1,ClassId1,ProjName2, ClassId2)">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
