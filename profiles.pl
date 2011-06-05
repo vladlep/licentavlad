b80bd18f11d81ec56892a1ea0756e4218915b0e7
vlad.lep <vlad.lep@122ac476-ae23-22c3-0b7d-92a7310358e7>
2011-06-05 10:59:51 +0000

    fisierul de profile

--- profiles.pl
+++ profiles.pl
@@ -1,17 +1,27 @@
 %-----------------------------------------%
-%1 = loose profile
-classDelta(1,[
-	      5 % delta atributes
-	   ,0.6 % procent atributes
+%loose profile
+projectDelta(loose,
+	     [
+	      1 % MaxDeltaClasses- diferenta maxima intre nr de clase din primul si cel de-al doilea proiect
 
-	     ,2 % delta methods
-	   ,0.7 % procent methods
-	      ,1 % delta Interfaces
-	  ]).
+	     ]).
+classDelta(loose, % profil larg, care genereaza mai multe match-uri	   [
+	   [
+	    5  % delta atributes
+	   ,0.6% procent atributes
 
-methodDelta(1,[
+	   ,2  % delta methods
+
+	   ,1  % delta Interfaces
+	   ,0.5%ProcentMatchHigh-min procent of method matches for classe to be high
+	   ,1  %MaxNumberOfUnmathcingMethods  - dif. max intre match si nr clase din fiecare proiect
+	       % pentru a declara o clasa medium
+	   ,0.5%ProcentMinOfMatchingMethods - mi procent pt a declara o clasa med.
+	   ]).
+
+methodDelta(loose,
+	    [
 	      2 %delta if
-	       ,2
 	      ]).
 
 %-----------------------------------------%
