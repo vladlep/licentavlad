fe520979910593502cbc51b8e1e0153ff9c773ef
vlad.lep <vlad.lep@122ac476-ae23-22c3-0b7d-92a7310358e7>
2011-06-08 21:49:33 +0000

    adaugat profiles.pl tight

--- profiles.pl
+++ profiles.pl
@@ -15,7 +15,7 @@ classDelta(loose, % profil larg, care genereaza mai multe match-uri	   [
 	   ,0.5%ProcentMatchHigh-min procent of method matches for classe to be high
 	   ,1  %MaxNumberOfUnmathcingMethods  - dif. max intre match si nr clase din fiecare proiect
 	       % pentru a declara o clasa medium
-	   ,0.5%ProcentMinOfMatchingMethods - mi procent pt a declara o clasa med.
+	   ,0.5%ProcentMinOfMatchingMethods - min procent pt a declara o clasa med.
 	   ]).
 
 methodDelta(loose,
@@ -26,3 +26,26 @@ methodDelta(loose,
 %-----------------------------------------%
 
 % tight profile. Projects have to be closer mathces
+projectDelta(tight,
+	     [
+	      0 % MaxDeltaClasses- diferenta maxima intre nr de clase din primul si cel de-al doilea proiect
+
+	     ]).
+classDelta(tight, % profil larg, care genereaza mai multe match-uri	   [
+	   [
+	    3  % delta atributes
+	   ,0.7% procent atributes
+
+	   ,1  % delta methods
+
+	   ,0  % delta Interfaces
+	   ,0.6%ProcentMatchHigh-min procent of method matches for classe to be high
+	   ,1  %MaxNumberOfUnmathcingMethods  - dif. max intre match si nr clase din fiecare proiect
+	       % pentru a declara o clasa medium
+	   ,0.5%ProcentMinOfMatchingMethods - min procent pt a declara o clasa med.
+	   ]).
+
+methodDelta(tight,
+	    [
+	      2 %delta if
+	      ]).
